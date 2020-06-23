0e4eea6f1196ac566e525048af6de10d67523fb7
Saira Rehman <saira@Sairas-MacBook-Pro.local>
2020-06-23 16:23:46 -0400

    syntax error resolved

--- js/index.js
+++ js/index.js
@@ -20,28 +20,13 @@ const $msgList = document.getElementById('messages')
 const $userName = document.getElementById('userName') // to get message input
 var newUser
 
-// -----To do-----------
-// sendButton.addEventListener('click', function(){
-// 	 if(message.value.length > 0 ){
-// 	socket.emit('chat', {
-// 		message: message.value,
-// 	});
-//   }
-//   //once the message is sent, reset the innerHTML of the message div to an empty string
-// 	message.value = "";
-//   });
-
-//   socket.on("chat",function(data){
-//     io.sockets.emit("chat",data)
-//   });
-  
 
 
 $msgForm.addEventListener('submit', (event) => {
 	event.preventDefault()
 	const message = messageInput.value    // get the value from the message     
 	socket.emit('send-chat-message', message)   // send info from client to server
-	messageInput.vlaue = ''      // empty out message each time we sent it
+	messageInput.value = ''      // empty out message each time we sent it
 
 	socket.emit('chatmsg', {msg: event.currentTarget.txt.value})
 	event.currentTarget.txt.value = ''
