56928591db91fc8effa0a83704cc29930c235d0c
Saira Rehman <saira@Sairas-MacBook-Pro.local>
2020-06-30 02:34:48 -0400

    box and font size increased

--- js/index.js
+++ js/index.js
@@ -15,12 +15,15 @@ socket.on('newuser', (data) => console.log(`${data.user} has connected!`))
 // Listen for "chatmsg"
 //   add a <li> with the chat msg to the <ol>
 
+
+// +++++Variable declaration for each element +++++
 const $msgForm = document.getElementById('sendMsg')
 const $msgList = document.getElementById('messages')
 const $userName = document.getElementById('userName') // to get message input
 const $chatName = document.getElementById("chatName")
 var newUser
 
+// +++++Eventlistener for New user, execute whenever SUBMIT EVENT occurs+++++
 $userName.addEventListener('submit',(event)=>{
 	event.preventDefault();
 	newUser=event.currentTarget.user.value;
@@ -31,17 +34,23 @@ $userName.addEventListener('submit',(event)=>{
 )
 
 socket.on('username', (data) =>{
-	console.log(`By : ${data.name}`)
-	$chatName.innerHTML = `By : ${data.name}`
+	console.log(`Welcome: ${data.name}`)
+	$chatName.innerHTML = ` Welcome: ${data.name}`
 })
+//+++++ NEW USER functionality ends here +++++
 
+
+// +++++ Event listener for incoming message +++++
 $msgForm.addEventListener('submit', (event) => {
 	event.preventDefault()
 	const message = messageInput.value    // get the value from the message     
 	socket.emit('send-chat-message', message)   // send info from client to server
 	messageInput.value = ''      // empty out message each time we sent it
 
+
+
 	socket.emit('chatmsg', {msg: event.currentTarget.txt.value})
 	event.currentTarget.txt.value = ''
-})
+});
 
+// +++++ Incoming MESSAGE functionality ends here +++++
